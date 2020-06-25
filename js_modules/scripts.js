dd509855b70b18cbf2e241ff8890fae2a6b2c51a
Nikita <bbusinesspunk@gmail.com>
2020-06-26 01:28:16 +0300

    Had wrote a module

--- js_modules/scripts.js
+++ js_modules/scripts.js
@@ -1,37 +1,16 @@
+import "@babel/polyfill";
+import Timer from './timer';
 
 window.addEventListener('load', function(){
     let timer1 = new Timer(document.querySelector('.timer1'), 10);
-    console.log(timer1);
 });
 
-class Timer{
-    constructor(el, time){
-        this.el = el;
-        this.time = time;
-        this._interval;
+import {wordsCount, getWords} from './stringer';
 
-        this.render();
-        this.start();
-    }
+let string = '  Всем  привет! Ура ура! ';
 
-    start(){
-        this._interval = window.setInterval(this.tick, 1000);
-    }
+console.log( wordsCount(string) );
 
-    stop(){
-        window.clearInterval(this._interval);
-    }
-
-    tick = () => {
-        this.time--;
-        this.render();
-
-        if(this.time <= 0){
-            this.stop();
-        }
-    }
-
-    render(){
-        this.el.innerHTML = this.time;
-    }
+for ( let word of getWords(string)) {
+    console.log(word);
 }
